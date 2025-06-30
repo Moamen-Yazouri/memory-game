4518c395387f1f1555e807b2fd53f448fb73f82e
Moamen-Yazouri <moaamenalyazouro@gmail.com>
2025-06-30 10:16:24 +0300

    feat(game): add card flipping logic and id to game cards
    - Add id field to IGameCard interface and pass it through components
    - Implement card flipping logic in reducer with matching and timeout
    - Update eslint config to disable empty object type rule

--- src/states/game-state/reducer.tsx
+++ src/states/game-state/reducer.tsx
@@ -9,7 +9,8 @@ export interface IState {
 }
 
 export type Action = 
-    {type: "INITIAL_GAME", payload: LevelsTypes}
+    {type: "INITIAL_GAME", payload: LevelsTypes} |
+    {type: "FLIPP_CARD", payload: number}
 
 export const reducer = (state: IState, action: Action) => {
     switch(action.type) {
@@ -18,6 +19,63 @@ export const reducer = (state: IState, action: Action) => {
             return {
                 ...state,
                 cards
+            }
+        }
+
+        case "FLIPP_CARD": {
+            let opened = [...state.openCards];
+            const selected = state.cards.find((c) => {
+                c.id === action.payload;
+            });
+            opened.push(selected!);
+
+            if(opened.length == 2) {
+                const c1 = opened[0];
+                const c2 = opened[1];
+
+                if(c1.value == c2.value) {
+                    const newCards = state.cards.map((c) => (
+                        c.id == c1.id || c.id == c2.id 
+                        ? {...c, isMatched: true}
+                        : c
+                    ));
+                    opened = [];
+                    return {
+                        ...state,
+                        cards: newCards,
+                        openCards: opened, 
+                    };
+                }
+                else {
+                    setTimeout(() => {
+                        const newCards = state.cards.map((c) => (
+                            c.id == c1.id || c.id == c2.id 
+                                ? {...c, isFlipped: false}
+                                : c
+                        ));
+
+                        opened = [];
+                        return {
+                            ...state,
+                            cards: newCards,
+                            openCards: opened,
+                            wrongMoves: state.wrongMoves +1
+                        }
+                    }, 500);
+                }
+            }
+
+            else {
+                const newCards = state.cards.map((c) => (
+                    opened[0].id == c.id 
+                        ? {...c, isFlipped: true}
+                        : c
+                ));
+                return {
+                    ...state, 
+                    openCards: opened,
+                    cards: newCards,
+                }
             }
         }
     }
